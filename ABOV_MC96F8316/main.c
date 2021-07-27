679ab6a87e01d803b36034c39efcf1207c370542
jongwooksi <tgs03087@naver.com>
2021-07-27 14:34:33 +0900

    insert MC96F3816 Code ( from 332A)

--- ABOV_MC96F8316/main.c
+++ ABOV_MC96F8316/main.c
@@ -9,19 +9,20 @@
 // Generated    : Tue, Jul 27, 2021 (11:47:04)
 #include	"MC96F8316.h"
 #include	"func_def.h"
+#include <stdio.h>
+#include <string.h>
 
-void main()
-{
-	cli();          	// disable INT. during peripheral setting
-	port_init();    	// initialize ports
-	clock_init();   	// initialize operation clock
-	UART_init();    	// initialize UART interface
-	sei();          	// enable INT.
-	
-	// TODO: add your main code here
-	
-	while(1);
-}
+
+#define PWM0_FLAG 	0x01
+#define PWM1_FLAG 	0x02
+#define PWM2_FLAG 	0x04
+#define PWM3_FLAG 	0x08
+
+#define PWM_CTL_OFF		0
+#define PWM_CTL_INC		1
+#define PWM_CTL_DEC		2
+
+#define PWM_CH_NUM		4
 
 //======================================================
 // interrupt routines
@@ -31,14 +32,217 @@ void main()
 // peripheral setting routines
 //======================================================
 
-unsigned char UART_read()
+
+typedef struct _tagPWMINFO {
+	int 	period;
+	int 	duty;
+	int 	default_duty;
+	int 	next_duty;
+	int 	count;
+	int		out;
+} PWMINFO;
+
+
+
+static unsigned int _pwm_ctl_dec_flag = 0;
+static unsigned int _pwm_ctl_inc_flag = 0;
+static unsigned int _pwm_ctl_cnt = 0;
+
+static unsigned int _pwm_flag_list[PWM_CH_NUM] = { PWM0_FLAG, PWM1_FLAG, PWM2_FLAG, PWM3_FLAG };
+
+
+
+// ----------------------------------
+static int _pwm_mask = 0x0f;	// Enable Mask : - - - - PWM3 PWM2 PWM1 PWM0
+PWMINFO 	_pwm[PWM_CH_NUM];
+
+
+static char _pwm_out;
+
+void pwm_setup( PWMINFO *pwm, int period, int duty )
 {
-	unsigned char dat;
-	
-	while(!(UARTST & 0x20));	// wait
-	dat = UARTDR;   	// read
-	return	dat;
+	pwm->period 		= period;
+	pwm->duty   		= duty;
+	pwm->next_duty  	= duty;
+	pwm->default_duty	= duty;
+	pwm->count  	= 0;
+	pwm->out 		= 0;
 }
+
+
+void pwm_init()
+{
+
+	pwm_setup( &_pwm[0], 10, 0 );
+	pwm_setup( &_pwm[1], 10, 7 );
+	pwm_setup( &_pwm[2], 10, 8 );
+	pwm_setup( &_pwm[3], 10, 9 );
+
+}
+
+void pwm_setup_control( unsigned int pwmflag, unsigned int ctl )
+{
+	if( ctl == PWM_CTL_OFF ) 
+	{
+		_pwm_ctl_inc_flag &= ~pwmflag;
+		_pwm_ctl_dec_flag &= ~pwmflag;
+	}
+	else if( ctl == PWM_CTL_DEC ) 
+	{
+		_pwm_ctl_dec_flag |=  pwmflag;
+		_pwm_ctl_inc_flag &= ~pwmflag;
+	}
+	else 
+	{
+		_pwm_ctl_dec_flag &= ~pwmflag;
+		_pwm_ctl_inc_flag |=  pwmflag;
+	}
+}
+
+
+//======================================================
+void pwm_control_Motor()
+{
+
+	// PWM Motor
+	if( _pwm[0].count < _pwm[0].duty && (_pwm_mask & PWM0_FLAG) ) {
+		P3 = 0x20;	// P35
+	}
+
+	_pwm[0].count++;
+
+	
+	if( _pwm[0].count >= _pwm[0].period ) {
+		_pwm[0].count = 0;
+		_pwm[0].duty  = _pwm[0].next_duty;
+		P3 = 0x00;
+	}
+
+}
+
+
+void pwm_control_LED()
+{
+	// PWM 1
+	if( _pwm[1].count < _pwm[1].duty && (_pwm_mask & PWM1_FLAG) ) {
+		_pwm_out |= 0x02;	// P11
+	}
+
+	_pwm[1].count++;
+
+	if( _pwm[1].count >= _pwm[1].period ) {
+		_pwm[1].count = 0;
+		_pwm[1].duty  = _pwm[1].next_duty;
+	}
+
+
+	// PWM 2
+	if( _pwm[2].count < _pwm[2].duty && (_pwm_mask & PWM2_FLAG) ) {
+		_pwm_out |= 0x04;	// P12
+	}
+
+	_pwm[2].count++;
+
+	if( _pwm[2].count >= _pwm[2].period ) {
+		_pwm[2].count = 0;
+		_pwm[2].duty  = _pwm[2].next_duty;
+	}
+
+
+	// PWM 3
+	if( _pwm[3].count < _pwm[3].duty && (_pwm_mask & PWM3_FLAG) ) {
+		_pwm_out |= 0x08;	// P13
+	}
+
+	_pwm[3].count++;
+
+	
+	if( _pwm[3].count >= _pwm[3].period ) {
+		_pwm[3].count = 0;
+		_pwm[3].duty  = _pwm[3].next_duty;
+	}
+
+
+	P1 = _pwm_out;
+
+	
+}
+
+void pwm_enable( int pwm0, int pwm1, int pwm2, int pwm3 )
+{
+	// 0:Disable 1:Enable -1:Don't care(not change)
+
+	// B/G/R/Motor
+	if( pwm0 != -1 ) { if( pwm0 ) _pwm_mask |= PWM0_FLAG; else _pwm_mask &= ~PWM0_FLAG; }
+	if( pwm1 != -1 ) { if( pwm1 ) _pwm_mask |= PWM1_FLAG; else _pwm_mask &= ~PWM1_FLAG; }
+	if( pwm2 != -1 ) { if( pwm2 ) _pwm_mask |= PWM2_FLAG; else _pwm_mask &= ~PWM2_FLAG; }
+	if( pwm3 != -1 ) { if( pwm3 ) _pwm_mask |= PWM3_FLAG; else _pwm_mask &= ~PWM3_FLAG; }
+}
+
+//======================================================
+void setupOpMode()
+{
+	pwm_setup_control( PWM0_FLAG, PWM_CTL_OFF );
+	pwm_setup_control( PWM1_FLAG, PWM_CTL_OFF );
+	pwm_setup_control( PWM2_FLAG, PWM_CTL_OFF );
+	pwm_setup_control( PWM3_FLAG, PWM_CTL_OFF );
+
+}
+
+
+void setMotorPWM()
+{	
+
+	if(UARTDR == 0x00)
+	{
+		pwm_enable(0, -1, -1, -1);
+		UART1_Clear();
+	}
+			
+	else if(UARTDR == 0x01)
+	{
+		pwm_enable( 1, -1,-1,-1 );
+		pwm_setup( &_pwm[0], 10, 6 );
+		UART1_Clear();
+	}
+		
+	else if(UARTDR == 0x02)
+	{
+		pwm_enable( 1, -1,-1,-1 );
+		pwm_setup( &_pwm[0], 10, 7 );
+		UART1_Clear();
+	}	
+
+	else if(UARTDR == 0x03)
+	{
+		pwm_enable( 1, -1,-1,-1 );
+		pwm_setup( &_pwm[0], 10, 8 );
+		UART1_Clear();
+	}	
+	
+	else if(UARTDR == 0x04)
+	{
+		pwm_enable( 1, -1,-1,-1 );
+		pwm_setup( &_pwm[0], 10, 9 );
+		UART1_Clear();
+	}	
+
+	
+	else if(UARTDR == 0x05)
+	{
+		pwm_enable( 1, -1,-1,-1 );
+		pwm_setup( &_pwm[0], 10, 10 );
+		UART1_Clear();
+	}
+}
+
+
+void UART1_Clear()
+{
+	UARTDR = 0xFF;
+}
+
+
 
 void UART_init()
 {
@@ -49,13 +253,11 @@ void UART_init()
 	UARTCR2 |= 0x0D;	// interrupt, speed
 	UARTCR3 = 0x00; 	// stop bit
 	UARTBD = 0x0C;  	// baud rate
+	UARTST=0x60;	  //UDRE TXC RXC WAKE SOFTRST DOR FE PE 
+
 }
 
-void UART_write(unsigned char dat)
-{
-	while(!(UARTST & 0x80));	// wait
-	UARTDR = dat;   	// write
-}
+
 
 void clock_init()
 {
@@ -102,5 +304,36 @@ void port_init()
 	P1FSRL = 0x00;  	// P1 selection Low
 	P2FSR = 0x00;   	// P2 selection
 	P3FSR = 0x01;   	// P3 selection
+}
+
+
+
+void main()
+{
+	cli();          	// disable INT. during peripheral setting
+	port_init();    	// initialize ports
+	clock_init();   	// initialize operation clock
+	UART_init();    	// initialize UART interface
+	pwm_enable( 1, 1, 1, 1 );	
+	pwm_init();
+	
+	sei();          	// enable INT.
+	
+	// TODO: add your main code here
+	UARTST = 0xFF;
+	
+	while(1)
+	{		
+		for (i= 0; i < 10; i++)
+			pwm_control();
+		
+		if (UARTST < 0x06)
+		{
+			setMotorPWM();
+		}
+		
+		
+		
+	}
 }
 
